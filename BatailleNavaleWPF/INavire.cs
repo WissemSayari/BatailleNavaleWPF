953e53f5616c63d6ea005ad8a4d0aa53784bf6cc
Wissem Sayari <2094293@cegeplimoilou.ca>
2022-10-04 15:35:15 -0400

    Ajout de l'interface INavire

---
+++ BatailleNavaleWPF/INavire.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatailleNavale
+{
+    interface INavire
+    {
+         TypeNavire Type { get; }
+        bool Coule { get; }
+
+    }
+}
